ac6e8a617b6e8f9c6c14d39bcada44dfc84a318a
Galichev Anton <galichev@gmail.com>
2015-11-07 04:08:57 +0300

    Jivosite support for online customers

--- track-common/v1.7/track-show/templates/footer.inc.php
+++ track-common/v1.7/track-show/templates/footer.inc.php
@@ -9,3 +9,12 @@
 
 <!-- Include local script calls -->  
 <script src="<?php echo _HTML_LIB_PATH;?>/bootstrap/js/custom.js"></script>
+
+<style>
+    #jivo_chat_widget{display: none;}
+    </style>
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+    (function(){ var widget_id = 'fylD9zYa3M';
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
--- track-common/v1.7/track-show/templates/menu-top.inc.php
+++ track-common/v1.7/track-show/templates/menu-top.inc.php
@@ -51,6 +51,7 @@ if (count($arr_timezone_settings) == 0) {
                 <li <?php if (isset($_REQUEST['page']) && in_array($_REQUEST['page'], array('import', 'postback'))) {
     echo 'class="active"';
 } ?>><a href="?page=import">Инструменты</a></li>
+                <li><a href="#" onclick="jivo_api.setContactInfo({email : '<?php echo $auth_info[1];?>'}); jivo_api.open(); return false;">Поддержка</a></li>
             </ul>
 
             <!-- New offer form-->
